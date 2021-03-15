37d999e44fa2811abf001d757462dedccdce19ee
Akarsh Malik <malikakarsh@gmail.com>
2021-03-15 06:40:01 +0530

    [Crypto Challenge] Secret Is Secret

--- Secret Is Secret/index.php
+++ Secret Is Secret/index.php
@@ -39,7 +39,7 @@
         <div class="div3">
             <div class="div2">
             <h1 class="space">Test if server is up!</h1>
-            <form action="" method="GET">
+            <form action="" method="POST">
                 <select name="connect_to" class="space">
                     <option value="admin1">Admin1</option>
                     <option value="admin2">Admin2</option>
@@ -48,13 +48,16 @@
                 <button class="space" style="padding: 10px 20px 10px 20px;" type="submit" name="submit">CHECK</button>
             </form>
             <?php
-            if (isset($_GET['submit'])){
-            $command=$_GET['connect_to']; 
-            if ($_GET['connect_to']=='admin1'){
+            if (isset($_POST['submit'])){
+            $command=$_POST['connect_to']; 
+            if ($_POST['connect_to']=='admin1'){
                 echo "<h3 style='color: red;padding-top: 10px;'>Down!</h3>";
             }
-            else if ($_GET['connect_to']=='admin3'){
+            else if ($_POST['connect_to']=='admin3'){
                 echo "<h3 style='color: red;padding-top: 10px;'>Down!</h3>";
+            }
+            else if (preg_match('/index.php/',$command) || preg_match('/index.html/',$command)){
+                echo "";
             }
             else{
             $cmd="ping -c 2 $command";
@@ -67,7 +70,7 @@
             </div>
             <div class="div1">
             <h1 class="space">Admin Login</h1>
-            <form action="login.php" method="POST">
+            <form action="" method="POST">
                 <input class="space" type="text" name="name" placeholder="name" required>
                 <br>
                 <input class="space" type="password" name="password" placeholder="password" required>
@@ -80,6 +83,32 @@
                 $password=$_POST['password'];
                 if ($name=='admin2' && $password=='itsAsecret'){
                     echo "<h3 style='padding-top: 10px;'>Login Successful</h3>";
+                    echo "<table>
+  <tr>
+    <th>Date</th>
+    <th>Log</th>
+  </tr>
+  <tr>
+    <td>01/04/21</td>
+    <td>eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4xIiwiYWRtaW4iOnRydWUsImp0aSI6ImYwZGQ2MjliLWNkNmItNGM4OC1iNzgzLWE0NjhjNDliZGRjNCIsImlhdCI6MTYxNTc2ODg4OSwiZXhwIjoxNjE1NzcyNDg5fQ.tJJJQEtDY37mpaKT1WPqcEsyB9136DWhxp1-LZlIDNg</td>
+  </tr>
+  <tr>
+    <td>04/04/21</td>
+    <td>eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4zIiwiYWRtaW4iOnRydWUsImp0aSI6ImM4YzhlOWZhLTQ4OTEtNGY5My05YmQ3LWIwNWNmNDc3MjNmZCIsImlhdCI6MTYxNTc2OTAyOCwiZXhwIjoxNjE1NzcyNjI4fQ.3OJdIdI_aAv7UvJg5iYJxa1tkalgQV-BJtxJEUvH774</td>
+  </tr>
+   <tr>
+    <td>07/04/21</td>
+    <td>eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4xIiwiYWRtaW4iOnRydWUsImp0aSI6ImYwZGQ2MjliLWNkNmItNGM4OC1iNzgzLWE0NjhjNDliZGRjNCIsImlhdCI6MTYxNTc2ODg4OSwiZXhwIjoxNjE1NzcyNDg5fQ.tJJJQEtDY37mpaKT1WPqcEsyB9136DWhxp1-LZlIDNg</td>
+  </tr>
+   <tr>
+    <td>15/04/21</td>
+    <td>eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4yIiwiYWRtaW4iOnRydWUsImp0aSI6IjViOTBlNTA3LWU4ZTMtNDdjYy1iY2QxLWZiZGI0ZDE2NzQ0NyIsImlhdCI6MTYxNTc2OTEwNywiZXhwIjoxNjE1NzcyNzA3fQ.KC4wOTtZpC2VtOMtN5BFn-6RvY7cYrBwr93ooGwlprg</td>
+  </tr>
+    <tr>
+    <td>18/04/21</td>
+    <td>eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiaGFja2VyIiwiZmxhZyI6InBfY3Rme2NyNHp5X2NyeXB0MF8wbl93M2J9IiwianRpIjoiNWY2ZTIxNjEtZTMyNS00MzI4LTg0ZTctZGRkZWQwMWNiMjgwIiwiaWF0IjoxNjE1NzY5MjY3LCJleHAiOjE2MTU3NzI4Njd9.4kXjstpn1jIP8bqGm4FMxEV0LVWnxB4eqQh1JML94_k</td>
+  </tr>
+</table>";
                 }
                 else{
                     echo "<h3 style='color: red;padding-top: 10px;'>Invalid Credentials</h3>";
